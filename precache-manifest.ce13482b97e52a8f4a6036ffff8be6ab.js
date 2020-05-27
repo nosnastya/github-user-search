f3d69abf6c1711177d949250e05da61ba98bea23
Anastasiia Nos <nosnastya@gmail.com>
2020-05-27 12:16:21 +0400

    Updates

---
+++ precache-manifest.ce13482b97e52a8f4a6036ffff8be6ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff8e044fbe1f28309c94e61b2f9cc962",
+    "url": "/github-user-search/index.html"
+  },
+  {
+    "revision": "eea748638b9084e4bb05",
+    "url": "/github-user-search/static/css/main.708a5f04.chunk.css"
+  },
+  {
+    "revision": "38b40c39e0033203be9b",
+    "url": "/github-user-search/static/js/2.6a01f91b.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/github-user-search/static/js/2.6a01f91b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eea748638b9084e4bb05",
+    "url": "/github-user-search/static/js/main.18a24fe2.chunk.js"
+  },
+  {
+    "revision": "d15723f1aabdbe10e70e",
+    "url": "/github-user-search/static/js/runtime-main.2e993643.js"
+  },
+  {
+    "revision": "774660632e2a9e6708a2ceed5126a5e2",
+    "url": "/github-user-search/static/media/octocat-spinner-128.77466063.gif"
+  }
+]);
